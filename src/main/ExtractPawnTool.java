1520b23fdb1a3c61d73386717596fb9e138dfe2c
MomemtumMori <admin@momemtummori.info>
2013-07-09 17:18:50 -0700

    The extractor now reads directly from a file.

--- src/main/ExtractPawnTool.java
+++ src/main/ExtractPawnTool.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
@@ -7,7 +11,7 @@ import parser.*;
 
 public class ExtractPawnTool {
 	public static void main(String[] args) throws Exception {
-		ANTLRInputStream input = new ANTLRInputStream(System.in);
+		ANTLRInputStream input = new ANTLRInputStream(getFileContent(args[0]).toString());
 		// create a lexer that feeds off of input CharStream
 		SPOTLexer lexer = new SPOTLexer(input);
 		// create a buffer of tokens pulled from the lexer
@@ -20,5 +24,32 @@ public class ExtractPawnTool {
 		ExtractPawnListener extractor = new ExtractPawnListener(parser);
 		walker.walk(extractor, tree); // initiate walk of tree with listener
 		System.out.println(extractor.getOutput());
+	}
+	
+	private static StringBuffer getFileContent(String path) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = null;
+ 
+		try { 
+			String sCurrentLine; 
+			br = new BufferedReader(new FileReader(path));
+ 
+			while ((sCurrentLine = br.readLine()) != null) {
+				sb.append(sCurrentLine);
+				sb.append("\n");
+			}
+ 
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				throw ex;
+			}
+		}
+		
+		return sb;
 	}
 }
